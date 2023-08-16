a716d584ceb894ea7dd7574c5c4156f4377ba354
Matthew Yacobucci <m.yacobucci@f5.com>
2023-08-16 14:30:34 -0600

    feat: Howto example to accompany ngx-rust blog
    Simple Rust module to serve as a howto.
    
    Add the "howto" directive with a HTTP method as an argument.
    
    The configured method will be allowed, and all others will fail.

--- README.md
+++ README.md
@@ -1,59 +1,32 @@
-[![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
-[![Community Support](https://badgen.net/badge/support/community/cyan?icon=awesome)](https://github.com/nginxinc/f5yacobucci/ngx-rust-howto/blob/main/SUPPORT.md)
-<!-- [![Commercial Support](https://badgen.net/badge/support/commercial/cyan?icon=awesome)](<Insert URL>) -->
-
-# NGINX Template Repository
-
-## What is included on this template?
-
-This template includes all the scaffolding you need to get started on a standards compliant NGINX repository:
-
-- Standard license for NGINX OSS projects
-- Standard `.gitignore` with minimal defaults
-- Issue and PR templates
-- Contributing guidelines
-- Support guidelines
-- Security guidelines for reporting major vulnerabilities
-- NGINX Code of Conduct
-- README placeholder. How you structure the README is up to you (although the template provides placeholder sections), but you will need to include:
-  - A [repostatus](https://www.repostatus.org/) badge
-  - A community and commercial support badge. Include the latter -- and replace the URL placeholder with the relevant support URL -- if this repository contains a commercially supported project. You can find a commented out example below the community badge in this README.
-  - An explicit link back to the [Apache License 2.0](https://github.com/nginxinc/template-repository/blob/main/LICENSE)
-  - An up to date copyright notice
-- Changelog placeholder. (Optional -- A changelog is recommended, but it is not required and can diverge in format from the placeholder here included.)
-- Codeowners placeholder. (Optional -- Codeowners is a useful GitHub feature, but not all repositories require them.)
-
-## How do I use this template?
-
-**DO NOT FORK** -- this template is meant to be used from the **[`Use this template`](https://github.com/nginxinc/template-repository/generate)** feature.
-
-1. Click on **[`Use this template`](https://github.com/nginxinc/template-repository/generate)**
-2. Give a name to your project
-3. Wait until the first run of CI finishes (GitHub Actions will process the template and commit to your new repo)
-4. Clone your new project and tweak any of the placeholders if necessary. Pay special attention to the README!
-5. Happy coding!
-
-**NOTE**: **WAIT** until the first CI run on GitHub Actions finishes before cloning your new project.
-
----
-
-<!--  DELETE THE LINES ABOVE THIS AND WRITE YOUR PROJECT README BELOW -- PLACEHOLDER SECTIONS HAVE BEEN INCLUDED FOR YOUR CONVENIENCE -->
-
 [![Project Status: Concept – Minimal or no implementation has been done yet, or the repository is only intended to be a limited example, demo, or proof-of-concept.](https://www.repostatus.org/badges/latest/concept.svg)](https://www.repostatus.org/#concept)
 
 # ngx_rust_howto
 
-## Requirements
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elit turpis, varius et arcu elementum, viverra rhoncus sem. Aliquam nec sodales magna, et egestas enim. Mauris lobortis ultrices euismod. Pellentesque in arcu lacus. Mauris cursus laoreet nulla, ac vehicula est. Vestibulum eu mauris quis lorem consectetur aliquam ac nec quam. Vestibulum commodo pharetra mi, at bibendum neque faucibus ut. Mauris et tortor sed sem consectetur eleifend ut non magna. Praesent feugiat placerat nibh, varius viverra orci bibendum sed. Vestibulum dapibus ex ut pulvinar facilisis. Quisque sodales enim et augue tempor mattis. Suspendisse finibus congue felis, ac blandit ligula. Praesent condimentum ultrices odio quis semper. Nunc ultrices, nibh quis mattis pellentesque, elit nulla bibendum felis, quis dapibus erat turpis ac urna.
-
 ## Getting Started
 
-Duis sit amet sapien vel velit ornare vulputate. Nulla rutrum euismod risus ac efficitur. Curabitur in sagittis elit, a semper leo. Suspendisse malesuada aliquam velit, eu suscipit lorem vehicula at. Proin turpis lacus, semper in placerat in, accumsan non ipsum. Cras euismod, elit eget pretium laoreet, tortor nulla finibus tortor, nec hendrerit elit turpis ut eros. Quisque congue nisi id mauris molestie, eu condimentum dolor rutrum. Nullam eleifend elit ac lobortis tristique. Pellentesque nec tellus non mauris aliquet commodo a eu elit. Ut at feugiat metus, at tristique mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+### Build
 
-## How to Use
+`cargo build`
 
-Maecenas at vehicula justo. Suspendisse posuere elementum elit vel posuere. Etiam quis pulvinar massa. Integer tempor semper risus, vitae maximus eros ullamcorper vitae. In egestas, ex vitae gravida sodales, ipsum dolor varius est, et cursus lorem dui a mi. Morbi faucibus ut nisi id faucibus. Sed quis ullamcorper ex. In et dolor id nunc interdum suscipit.
+### Running
+
+A basic configuration file is provided in the `conf` directory. Once you've built the project you'll have a usable object in the `target/debug` directory, move this to your desired location and update the `conf/howto.con` file with the proper path and name.
+
+You can now start an NGINX instance and try the module. For example, if you use an argument of "GET", the module will allow all `GET` requests and deny others.
+
+```
+curl localhost:8080
+proxy passed to backend
+
+curl -X POST localhost:8080
+<html>
+<head><title>403 Forbidden</title></head>
+<body>
+<center><h1>403 Forbidden</h1></center>
+<hr><center>nginx/1.23.3</center>
+</body>
+</html>
+```
 
 ## Contributing
 
---
+++ src/lib.rs
@@ -0,0 +1,169 @@
+use ngx::ffi::{
+    nginx_version, ngx_array_push, ngx_command_t, ngx_conf_t, ngx_http_core_module,
+    ngx_http_handler_pt, ngx_http_module_t, ngx_http_phases_NGX_HTTP_ACCESS_PHASE,
+    ngx_http_request_t, ngx_int_t, ngx_module_t, ngx_str_t, ngx_uint_t, NGX_CONF_TAKE1,
+    NGX_HTTP_LOC_CONF, NGX_HTTP_MODULE, NGX_RS_HTTP_LOC_CONF_OFFSET, NGX_RS_MODULE_SIGNATURE,
+};
+use ngx::http::{HTTPModule, MergeConfigError, Method};
+use ngx::{
+    core,
+    core::{Status, NGX_CONF_ERROR},
+    http,
+};
+use ngx::{http_request_handler, ngx_log_debug_http, ngx_modules, ngx_null_command, ngx_string};
+use std::{
+    os::raw::{c_char, c_void},
+    slice,
+};
+
+struct Module;
+
+// Implement our HTTPModule trait, we're creating a postconfiguration method to install our
+// handler's Access phase function.
+impl http::HTTPModule for Module {
+    type MainConf = ();
+    type SrvConf = ();
+    type LocConf = ModuleConfig;
+
+    unsafe extern "C" fn postconfiguration(cf: *mut ngx_conf_t) -> ngx_int_t {
+        let htcf = http::ngx_http_conf_get_module_main_conf(cf, &ngx_http_core_module);
+
+        let h = ngx_array_push(
+            &mut (*htcf).phases[ngx_http_phases_NGX_HTTP_ACCESS_PHASE as usize].handlers,
+        ) as *mut ngx_http_handler_pt;
+        if h.is_null() {
+            return core::Status::NGX_ERROR.into();
+        }
+
+        // set an Access phase handler
+        *h = Some(howto_access_handler);
+        core::Status::NGX_OK.into()
+    }
+}
+
+// Create a ModuleConfig to save our configuration state.
+#[derive(Debug, Default)]
+struct ModuleConfig {
+    enabled: bool,
+    method: String,
+}
+
+// Implement our Merge trait to merge configuration with higher layers.
+impl http::Merge for ModuleConfig {
+    fn merge(&mut self, prev: &ModuleConfig) -> Result<(), MergeConfigError> {
+        if self.method.is_empty() {
+            self.method = String::from(if !prev.method.is_empty() {
+                &prev.method
+            } else {
+                ""
+            });
+        }
+        Ok(())
+    }
+}
+
+// Create our "C" module context with function entrypoints for NGINX event loop. This "binds" our
+// HTTPModule implementation to functions callable from C.
+#[no_mangle]
+static ngx_http_howto_module_ctx: ngx_http_module_t = ngx_http_module_t {
+    preconfiguration: Some(Module::preconfiguration),
+    postconfiguration: Some(Module::postconfiguration),
+    create_main_conf: Some(Module::create_main_conf),
+    init_main_conf: Some(Module::init_main_conf),
+    create_srv_conf: Some(Module::create_srv_conf),
+    merge_srv_conf: Some(Module::merge_srv_conf),
+    create_loc_conf: Some(Module::create_loc_conf),
+    merge_loc_conf: Some(Module::merge_loc_conf),
+};
+
+// Create our module structure and export it with the `ngx_modules!` macro. For this simple
+// handler, the ngx_module_t is predominately boilerplate save for setting the above context into
+// this structure and setting our custom configuration command (defined below).
+ngx_modules!(ngx_http_howto_module);
+
+#[no_mangle]
+pub static mut ngx_http_howto_module: ngx_module_t = ngx_module_t {
+    ctx_index: ngx_uint_t::max_value(),
+    index: ngx_uint_t::max_value(),
+    name: std::ptr::null_mut(),
+    spare0: 0,
+    spare1: 0,
+    version: nginx_version as ngx_uint_t,
+    signature: NGX_RS_MODULE_SIGNATURE.as_ptr() as *const c_char,
+
+    ctx: &ngx_http_howto_module_ctx as *const _ as *mut _,
+    commands: unsafe { &ngx_http_howto_commands[0] as *const _ as *mut _ },
+    type_: NGX_HTTP_MODULE as ngx_uint_t,
+
+    init_master: None,
+    init_module: None,
+    init_process: None,
+    init_thread: None,
+    exit_thread: None,
+    exit_process: None,
+    exit_master: None,
+
+    spare_hook0: 0,
+    spare_hook1: 0,
+    spare_hook2: 0,
+    spare_hook3: 0,
+    spare_hook4: 0,
+    spare_hook5: 0,
+    spare_hook6: 0,
+    spare_hook7: 0,
+};
+
+// Register and allocate our command structures for directive generation and eventual storage. Be
+// sure to terminate the array with the ngx_null_command! macro.
+#[no_mangle]
+static mut ngx_http_howto_commands: [ngx_command_t; 2] = [
+    ngx_command_t {
+        name: ngx_string!("howto"),
+        type_: (NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1) as ngx_uint_t,
+        set: Some(ngx_http_howto_commands_set_method),
+        conf: NGX_RS_HTTP_LOC_CONF_OFFSET,
+        offset: 0,
+        post: std::ptr::null_mut(),
+    },
+    ngx_null_command!(),
+];
+
+#[no_mangle]
+extern "C" fn ngx_http_howto_commands_set_method(
+    cf: *mut ngx_conf_t,
+    _cmd: *mut ngx_command_t,
+    conf: *mut c_void,
+) -> *mut c_char {
+    unsafe {
+        let conf = &mut *(conf as *mut ModuleConfig);
+        let args = (*(*cf).args).elts as *mut ngx_str_t;
+        conf.enabled = true;
+        conf.method = (*args.add(1)).to_string();
+    };
+
+    std::ptr::null_mut()
+}
+
+// Implement a request handler. Use the convenience macro, the http_request_handler! macro will
+// convert the native NGINX request into a Rust Request instance as well as define an extern C
+// function callable from NGINX.
+//
+// The function body is implemented as a Rust closure.
+http_request_handler!(howto_access_handler, |request: &mut http::Request| {
+    let co = unsafe { request.get_module_loc_conf::<ModuleConfig>(&ngx_http_howto_module) };
+    let co = co.expect("module config is none");
+
+    ngx_log_debug_http!(request, "howto module enabled called");
+
+    match co.enabled {
+        true => {
+            let method = request.method();
+
+            if method.as_str() == co.method {
+                return core::Status::NGX_OK;
+            }
+            http::HTTPStatus::FORBIDDEN.into()
+        }
+        false => core::Status::NGX_OK,
+    }
+});
